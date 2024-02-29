9b0460710b8583f6627bfd8dc006e710fbadcf34
Emma Åstrand <aastrand.emma@gmail.com>
2024-02-29 11:42:04 +0100

    Feat: added calculation

---
+++ src/main/java/com/github/aastrandemma/Calculation.java
@@ -0,0 +1,19 @@
+package com.github.aastrandemma;
+
+public class Calculation {
+    private static int addition(int first, int second) {
+        return first + second;
+    }
+
+    private static int subtraction(int first, int second) {
+        return first - second;
+    }
+
+    private static int multiplication(int first, int second) {
+        return first * second;
+    }
+
+    private static float division(int first, int second) {
+        return (float) (first / second);
+    }
+}
